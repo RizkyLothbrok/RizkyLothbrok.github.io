5f4106e2acedc6f8bfcba05910ca66698fbf6996
RizkyLothbrok <137852364+RizkyLothbrok@users.noreply.github.com>
2023-06-27 11:11:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const toggleButtons = document.querySelectorAll('.toggle-video');
-
-toggleButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        const video = this.nextElementSibling;
-        video.style.display = (video.style.display === 'none') ? 'block' : 'none';
-
-        const buttonText = this.textContent;
-        this.textContent = (buttonText === 'Show Video') ? 'Hide Video' : 'Show Video';
-    });
-});
+// Tidak ada perubahan yang diperlukan pada file script.js berdasarkan permintaan sebelumnya
